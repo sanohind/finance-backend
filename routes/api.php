effa70833703798928d5b41826df172edb3ecdb6
RizqiFarezi <rizqifarezi@gmail.com>
2025-01-21 17:03:15 -0800

    COMPLETE: complete adding middleware auth UserRole for handle every role that need Authentication

--- routes/api.php
+++ routes/api.php
@@ -13,27 +13,27 @@ use App\Http\Controllers\Api\SupplierFinance\SupplierInvLineController;
 Route::post('/login', [AuthController::class, 'login']);
 
 // Admin routes
-Route::middleware(['auth:sanctum', 'role:1'])->group(function () {
-    Route::get('/users', [UserController::class, 'index']);
-    Route::post('/users/store', [UserController::class, 'store']);
-    Route::get('/users/{id}/edit', [UserController::class, 'edit']);
-    Route::put('/users/update/{id}', [UserController::class, 'update']);
-    Route::delete('/users/delete/{id}', [UserController::class, 'destroy']);
-    Route::put('/users/{id}/status/{status}', [UserController::class, 'updateStatus']);
+Route::middleware(['auth:sanctum', 'userRole:1'])->prefix('super-admin')->group(function () {
+    Route::get('index', [UserController::class, 'index']);
+    Route::post('store', [UserController::class, 'store']);
+    Route::get('{id}/edit', [UserController::class, 'edit']);
+    Route::put('update/{id}', [UserController::class, 'update']);
+    Route::delete('delete/{id}', [UserController::class, 'destroy']);
+    Route::put('{id}/status/{status}', [UserController::class, 'updateStatus']);
 });
 
 // Finance routes
-Route::middleware(['auth:sanctum', 'role:2'])->group(function () {
-    Route::get('/finance/inv-header', [FinanceInvHeaderController::class, 'getInvHeader']);
-    Route::put('/finance/inv-header/{inv_no}', [FinanceInvHeaderController::class, 'update']);
-    Route::get('/finance/inv-line/{inv_no}', [FinanceInvLineController::class, 'getInvLine']);
-    Route::get('/files/{filename}', [FinanceInvDocumentController::class, 'streamFile']);
+Route::middleware(['auth:sanctum', 'userRole:2'])->prefix('finance')->group(function () {
+    Route::get('inv-header', [FinanceInvHeaderController::class, 'getInvHeader']);
+    Route::put('inv-header/{inv_no}', [FinanceInvHeaderController::class, 'update']);
+    Route::get('inv-line/{inv_no}', [FinanceInvLineController::class, 'getInvLine']);
+    Route::get('files/{filename}', [FinanceInvDocumentController::class, 'streamFile']);
 });
 
 // Supplier routes
-Route::middleware(['auth:sanctum', 'role:3'])->group(function () {
-    Route::get('/supplier/inv-header', [SupplierInvHeaderController::class, 'getInvHeader']);
-    Route::post('/supplier/inv-header/store', [SupplierInvHeaderController::class, 'store']);
-    Route::get('/supplier/inv-line', [SupplierInvLineController::class, 'getInvLineTransaction']);
-    Route::get('/supplier/inv-line/{inv_no}', [SupplierInvLineController::class, 'getInvLine']);
+Route::middleware(['auth:sanctum', 'userRole:3'])->prefix('supplier-finance')->group(function () {
+    Route::get('inv-header', [SupplierInvHeaderController::class, 'getInvHeader']);
+    Route::post('inv-header/store', [SupplierInvHeaderController::class, 'store']);
+    Route::get('inv-line', [SupplierInvLineController::class, 'getInvLineTransaction']);
+    Route::get('inv-line/{inv_no}', [SupplierInvLineController::class, 'getInvLine']);
 });
