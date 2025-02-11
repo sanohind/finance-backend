ce3e1f39fa12a19fe68291c3fa6ee4edf092efe7
RizqiFarezi <rizqifarezi@gmail.com>
2025-02-10 19:01:19 -0800

    ADD: add controller and function for SuperAdminInvLine and SuperAdminInvDocument

--- app/Http/Controllers/Api/Admin/SuperAdminInvLineController.php
+++ app/Http/Controllers/Api/Admin/SuperAdminInvLineController.php
@@ -1,14 +1,20 @@
 <?php
 
-namespace App\Http\Controllers\Api\Finance;
+namespace App\Http\Controllers\Api\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\InvLine;
 use App\Http\Resources\InvLineResource;
 
-class FinanceInvLineController extends Controller
+class SuperAdminInvLineController extends Controller
 {
+    public function getAllInvLine()
+    {
+        $invLines = InvLine::all();
+        return InvLineResource::collection($invLines);
+    }
+    
     public function getInvLine($inv_no)
     {
         $invLines = InvLine::where('inv_supplier_no', $inv_no)->get();
