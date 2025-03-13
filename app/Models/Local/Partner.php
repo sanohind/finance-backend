97c021d15ca07dcbfb19f9e8c3ba0d79ad911e5e
RizqiFarezi <rizqifarezi@gmail.com>
2025-03-12 18:06:50 -0700

    ADD: add bp_code on get invline for admin

--- app/Models/Local/Partner.php
+++ app/Models/Local/Partner.php
@@ -4,6 +4,8 @@ namespace App\Models\Local;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use App\Models\InvLine;
 
 class Partner extends Model
 {
@@ -12,5 +14,20 @@ class Partner extends Model
     protected $connection = "mysql2";
 
     protected $table = "business_partner";
-}
 
+    protected $primaryKey = "bp_code";
+
+    protected $keyType = "string";
+
+    protected $fillable = [
+        'bp_code',
+        'bp_name',
+        'bp_address',
+        'bp_email',
+    ];
+
+    public function invLine(): HasMany
+    {
+        return $this->hasMany(InvLine::class, 'bp_id', 'bp_code');
+    }
+}
