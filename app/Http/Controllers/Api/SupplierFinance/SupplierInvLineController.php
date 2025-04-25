34a688aeb1d9e2487eda3812e194945fb34f3496
RizqiFarezi <rizqifarezi@gmail.com>
2025-04-24 20:07:03 -0700

    try back

--- app/Http/Controllers/Api/SupplierFinance/SupplierInvLineController.php
+++ app/Http/Controllers/Api/SupplierFinance/SupplierInvLineController.php
@@ -34,26 +34,18 @@ class SupplierInvLineController extends Controller
         // Get the authenticated user's bp_code
         $bp_code = Auth::user()->bp_code;
 
-        // Get all uninvoiced lines for this supplier
-        // (Those with a receipt date but no invoice number assigned yet)
-        $invLines = InvLine::with('partner')
-            ->whereNotNull('actual_receipt_date')
-            ->whereNull('inv_supplier_no')
-            ->where('bp_id', $bp_code)
-            ->get();
-
         // Determine the cutoff date (10 days ago)
         $cutoffDate = Carbon::now()->subDays(10)->toDateString();
 
-        // Add new property "category" to each invoice line object
-        $invLines->each(function ($invLine) use ($cutoffDate) {
-            $receiptDate = Carbon::parse($invLine->actual_receipt_date);
+        // Get invoice lines where actual_receipt_date is <= cutoffDate
+        $invLines = InvLine::with('partner')
+            ->whereDate('actual_receipt_date', '<=', $cutoffDate)
+            ->where('bp_id', $bp_code)
+            ->get();
 
-            if ($receiptDate->toDateString() <= $cutoffDate) {
-                $invLine->category = "Danger, You Need To Invoicing This Item";
-            } else {
-                $invLine->category = "Safe, You Need To Invoicing In Time";
-            }
+        // Add new property "category" to each invoice line object
+        $invLines->each(function ($invLine) {
+            $invLine->category = "Danger, You Need To Invoicing This Item";
         });
 
         return InvLineResource::collection($invLines);
