1a21c71a57e8ccfb57a439e469562f85265e59ab
RizqiFarezi <rizqifarezi@gmail.com>
2025-07-25 09:41:42 +0700

    update job sync

--- app/Jobs/SyncInvoiceLinesDailyJob.php
+++ app/Jobs/SyncInvoiceLinesDailyJob.php
@@ -144,50 +144,6 @@ class SyncInvoiceLinesDailyJob implements ShouldQueue
                     ]);
 
                     // If record already existed, update it with latest data
-<<<<<<< Updated upstream
-                    if (!$invLine->wasRecentlyCreated) {
-                        $invLine->update([
-                            'bp_id' => $data->bp_id,
-                            'bp_name' => $data->bp_name,
-                            'currency' => $data->currency,
-                            'po_type' => $data->po_type,
-                            'po_reference' => $data->po_reference,
-                            'po_line' => $data->po_line,
-                            'po_sequence' => $data->po_sequence,
-                            'po_receipt_sequence' => $data->po_receipt_sequence,
-                            'actual_receipt_date' => $data->actual_receipt_date,
-                            'actual_receipt_year' => $data->actual_receipt_year,
-                            'actual_receipt_period' => $data->actual_receipt_period,
-                            'gr_no' => $data->gr_no,
-                            'packing_slip' => $data->packing_slip,
-                            'item_no' => $data->item_no,
-                            'ics_code' => $data->ics_code,
-                            'ics_part' => $data->ics_part,
-                            'part_no' => $data->part_no,
-                            'item_desc' => $data->item_desc,
-                            'item_group' => $data->item_group,
-                            'item_type' => $data->item_type,
-                            'item_type_desc' => $data->item_type_desc,
-                            'request_qty' => $data->request_qty,
-                            'actual_receipt_qty' => $data->actual_receipt_qty,
-                            'approve_qty' => $data->approve_qty,
-                            'unit' => $data->unit,
-                            'receipt_amount' => $data->receipt_amount,
-                            'receipt_unit_price' => $data->receipt_unit_price,
-                            'is_final_receipt' => $data->is_final_receipt,
-                            'is_confirmed' => $data->is_confirmed,
-                            'inv_doc_no' => $data->inv_doc_no,
-                            'inv_doc_date' => $data->inv_doc_date,
-                            'inv_qty' => $data->inv_qty,
-                            'inv_amount' => $data->inv_amount,
-                            'payment_doc' => $data->payment_doc,
-                            'payment_doc_date' => $data->payment_doc_date
-                        ]);
-                        Log::channel($this->logChannel)->debug("Updated existing record: po_no={$data->po_no}, receipt_no={$data->receipt_no}, receipt_line={$data->receipt_line}");
-                    } else {
-                        Log::channel($this->logChannel)->debug("Created new record: po_no={$data->po_no}, receipt_no={$data->receipt_no}, receipt_line={$data->receipt_line}");
-                    }
-=======
                     // if (!$invLine->wasRecentlyCreated) {
                     //     $invLine->update([
                     //         'bp_id' => $data->bp_id,
@@ -232,7 +188,6 @@ class SyncInvoiceLinesDailyJob implements ShouldQueue
                     // } else {
                     //     Log::channel($this->logChannel)->debug("Created new record: po_no={$data->po_no}, receipt_no={$data->receipt_no}, receipt_line={$data->receipt_line}");
                     // }
->>>>>>> Stashed changes
 
                     DB::commit();
                     $processedCount++;
